61af7ab0229b504aec01b9435e41f4531579e55d
gmail.com <sdfgdshjfghd@gmail.com>
2020-06-10 19:53:54 +0300

    add viber parse_request

--- weather_bot/views.py
+++ weather_bot/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
+from .viber_configure import viber
+
 
 class CallbackView(View):
     @method_decorator(csrf_exempt)
@@ -11,4 +13,6 @@ class CallbackView(View):
         return super(CallbackView, self).dispatch(*args, **kwargs)
 
     def post(self, request):
+        viber_request = viber.parse_request(request.body)
+        print(viber_request)
         return HttpResponse(status=200)
